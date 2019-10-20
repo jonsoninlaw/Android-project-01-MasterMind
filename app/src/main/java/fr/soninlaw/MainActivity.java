de32867746cb15091b5b829b8de98a16a543c378
soninlaw <jonsoninlaw@gmail.com>
2019-10-20 17:50:48 +0200

    Ajout d'une activity séparée pour entrer un nom de joueur

--- app/src/main/java/fr/soninlaw/MainActivity.java
+++ app/src/main/java/fr/soninlaw/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,126 +22,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView numbersShow = findViewById(R.id.playerGuess);
-        final Button button0 = findViewById(R.id.button0);
-        final Button button1 = findViewById(R.id.button1);
-        final Button button2 = findViewById(R.id.button2);
-        final Button button3 = findViewById(R.id.button3);
-        final Button button4 = findViewById(R.id.button4);
-        final Button button5 = findViewById(R.id.button5);
-        final TextView answerText = findViewById(R.id.answerText);
-        final TextView previousTries = findViewById(R.id.previousTries);
-        final Button restartButton = findViewById(R.id.restart_button);
+        final Button startButton = findViewById(R.id.startButton);
+        final EditText nameField = findViewById(R.id.nameField);
 
-        numbersShow.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (numbersShow.getText().toString().length() == 5) {
-                    int[] answer = new int[5];
-                    for (int i = 0; i < 5; i++) {
-                        answer[i] = Character.getNumericValue(numbersShow.getText().toString().charAt(i));
-                    }
-                    answerText.setText(master.compareMind(answer));
-                    previousTries.setText(numbersShow.getText().toString() + "   -->  bon(s) : " + master.getGoodNumbers() + "  placé(s) : " + master.getPlacedNumbers() + "\n" + previousTries.getText());
-
-                    if (master.isWin()) {
-                        restartButton.setVisibility(View.VISIBLE);
-                    }
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        button0.setOnClickListener(new View.OnClickListener() {
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numbersShow.getText().toString().length() >= 5) {
-                    numbersShow.setText("0");
+                if (nameField.getText().length() == 0) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Merci d'entrer au moins un caractère !", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.TOP| Gravity.CENTER, 0, 500);
+                    toast.show();
                 }
                 else {
-                    numbersShow.setText(numbersShow.getText().toString() + 0);
+                    Intent game = new Intent(MainActivity.this, GameActivity.class);
+                    game.putExtra("name", nameField.getText().toString());
+                    startActivity(game);
                 }
-            }
-        });
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numbersShow.getText().toString().length() >= 5) {
-                    numbersShow.setText("1");
-                }
-                else {
-                    numbersShow.setText(numbersShow.getText().toString() + 1);
-                }
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numbersShow.getText().toString().length() >= 5) {
-                    numbersShow.setText("2");
-                }
-                else {
-                    numbersShow.setText(numbersShow.getText().toString() + 2);
-                }
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numbersShow.getText().toString().length() >= 5) {
-                    numbersShow.setText("3");
-                }
-                else {
-                    numbersShow.setText(numbersShow.getText().toString() + 3);
-                }
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numbersShow.getText().toString().length() >= 5) {
-                    numbersShow.setText("4");
-                }
-                else {
-                    numbersShow.setText(numbersShow.getText().toString() + 4);
-                }
-            }
-        });
-
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numbersShow.getText().toString().length() >= 5) {
-                    numbersShow.setText("5");
-                }
-                else {
-                    numbersShow.setText(numbersShow.getText().toString() + 5);
-                }
-            }
-        });
-
-        restartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                restartButton.setVisibility(View.GONE);
-                master = new Master();
-                numbersShow.setText("");
-                answerText.setText("Try to do better !");
-                previousTries.setText("");
             }
         });
 
